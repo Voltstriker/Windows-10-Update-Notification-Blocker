06ee3b6faeda919a6401402d1f9e007c6b9188b1
Voltstriker <voltstriker@sonsofvalour.net>
2017-07-28 03:07:13 +1000

    Add file
    Signed-off-by: Voltstriker <voltstriker@sonsofvalour.net>

---
+++ Disable Windows 10 Update Notification.bat
@@ -0,0 +1,16 @@
+cd /d "%Windir%\System32"
+takeown /F MusNotification.exe
+icacls MusNotification.exe /deny Everyone:(X)
+takeown /F MusNotificationUx.exe
+icacls MusNotificationUx.exe /deny Everyone:(X)
+
+REM cd /d "%Windir%\System32"
+REM icacls MusNotification.exe /remove:d Everyone
+REM icacls MusNotification.exe /grant Everyone:F
+REM icacls MusNotification.exe /setowner "NT SERVICE\TrustedInstaller"
+REM icacls MusNotification.exe /remove:g Everyone
+REM icacls MusNotificationUx.exe /remove:d Everyone
+REM icacls MusNotificationUx.exe /grant Everyone:F
+REM icacls MusNotificationUx.exe /setowner "NT SERVICE\TrustedInstaller"
+REM icacls MusNotificationUx.exe /remove:g Everyone
+REM rem
